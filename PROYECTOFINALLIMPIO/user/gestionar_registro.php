7d6c76c5df508fee46ed5aa8b865c1764056ea2e
juankig <juankimone@hotmail.com>
2020-04-22 13:25:21 +0200

    correo

--- PROYECTOFINALLIMPIO/user/gestionar_registro.php
+++ PROYECTOFINALLIMPIO/user/gestionar_registro.php
@@ -1,10 +1,31 @@
 <?php
-require_once "_com/requireonces-comunes.php";
+
+require_once "../_com/requireonces-comunes.php";
+
+
+require_once "../_com/emailFuncion.php";
+
 $nombre=$_REQUEST["nombre"];
-$usuario=$_REQUEST["usuario"];
-$contrasenna=$_REQUEST["password"];
+$usuario=$_REQUEST["nombreUsuario"];
+$contrasenna=$_REQUEST["contrasenna"];
 $email=$_REQUEST["email"];
+
 DAO::clienteAgregarBD($nombre,$usuario,$contrasenna,$email);
-redireccionar("sesion-inicio.php");
+
+
+$cliente=DAO::usuarioObtenerPorUsuarioYContrasenna($usuario,$contrasenna);
+
+$asunto = 'Activar Cuenta - Sistema de Usuarios';
+$cuerpo=  'mundo';
+
+if(enviarEmail($email, $nombre, $asunto, $cuerpo)){
+
+    echo "Para terminar el proceso de registro siga las instrucciones que le hemos enviado la direccion de correo electronico: $email";
+    echo "<br><a href='sesion-inicio.php' >Iniciar Sesion</a>";
+    exit;
+} else {
+    redireccionar("registrarUsuario.php?incorrecto=true");
+}
+
 
 ?>
